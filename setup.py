34e38ee5a77cf94a267f5369cf99099c38a8a511
Keyi Zhang <keyi@stanford.edu>
2022-08-27 22:01:13 -0700

    add license

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires=[
         "kratos",
     ],
+    license_files=['LICENSE'],
     python_requires=">=3.6",
     extras_require={
         "test": ["pytest", "archipelago"],
